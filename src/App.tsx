5ce75310e0b6fd9b44fffab25492891b7261f5d2
rigolinux <rigoberto7567@gmail.com>
2023-03-14 09:42:59 -0600

    test react router and create project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Route,Routes } from "react-router-dom";
+
+import Login from './modules/login/views/Login';
+import User from './modules/user/views/User';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,17 +20,11 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<User />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// react-router-dom
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 )
