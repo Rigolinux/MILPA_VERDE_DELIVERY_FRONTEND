7a6127a2289eb862b5667bd4b930f05c473b98cd
paobonilla <beca19ola@gmail.com>
2023-04-26 16:09:26 -0600

    navbar que retorne a home

--- src/modules/NavBar/NavBar.tsx
+++ src/modules/NavBar/NavBar.tsx
@@ -8,7 +8,7 @@ const MyNavbar: React.FC = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Inicio</Nav.Link>
+          <Nav.Link href="/home">Inicio</Nav.Link>
           <Nav.Link href="#link">Acerca</Nav.Link>
           <NavDropdown title="Products" id="basic-nav-dropdown" menuVariant="dark">
             <NavDropdown.Item href="#action/3.1">Productos</NavDropdown.Item>
