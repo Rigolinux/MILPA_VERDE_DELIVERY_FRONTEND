1a1b08bda4d2b5d9abd2349cc6ef4576571667a1
NicoleMelendez <k.nicolemelendez@gmail.com>
2023-04-30 19:11:16 -0600

    NavBar changes

--- src/modules/NavBar/NavBar.tsx
+++ src/modules/NavBar/NavBar.tsx
@@ -4,14 +4,17 @@ import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 const MyNavbar: React.FC = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="/#">MilpaVerde</Navbar.Brand>
+      <Navbar.Brand href="/banner">MilpaVerde</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/banner">Inicio</Nav.Link>
+          <Nav.Link href="/providers">Providers</Nav.Link>
+          <Nav.Link href="/products">Productos</Nav.Link>
+          <Nav.Link href="/users">Users</Nav.Link>
           <Nav.Link href="/About">Acerca</Nav.Link>
-          <NavDropdown title="Productos" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Productos</NavDropdown.Item>
+          {/* <NavDropdown title="Productos" id="basic-nav-dropdown">
+            <NavDropdown.Item href="/products">Productos</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
               Another action
             </NavDropdown.Item>
@@ -22,7 +25,7 @@ const MyNavbar: React.FC = () => {
             <NavDropdown.Item href="#action/3.4">
               Separated link
             </NavDropdown.Item>
-          </NavDropdown>
+          </NavDropdown> */}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
