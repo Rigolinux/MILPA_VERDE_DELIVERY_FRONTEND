edefc6613c7a2dfe4d2aff8a7f61807951a7c2e8
paobonilla <beca19ola@gmail.com>
2023-05-01 21:26:19 -0600

    Modificando UsersAdd

--- src/modules/Users/views/UsersAdd.tsx
+++ src/modules/Users/views/UsersAdd.tsx
@@ -1,101 +1,113 @@
 import { TextField } from "@mui/material";
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import { Users } from "../../../interfaces/users";
 import { createUser } from "../../../api/users";
 import { useNavigate } from "react-router-dom";
 
 const UsersAdd = () => {
-
   const navigate = useNavigate();
 
   const [users, setUsers] = React.useState<Users>({
-    name: '',
-    lastname: '',
-    email: '',
-    password: '',
+    name: "",
+    lastname: "",
+    email: "",
+    password: "",
     // role: '',
-    username: '',
-  })
+    username: "",
+  });
 
-  const handlesubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    createUser(users).then((response) => {
-      console.log(response);
-      navigate('/users');
-    }).catch((error) => {
-      console.log(error);
-    });
+    createUser(users)
+      .then((response) => {
+        console.log(response);
+        navigate("/users");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
-    <div className="d-flex justify-content-center">
-      <div style={{ padding: '50px', }}>
-        <form onSubmit={handlesubmit}>
-
+    <Container className="d-flex justify-content-center align-items-center flex-column">
+      <h1 className="mb-4">Agregar usuario</h1>
+      <form onSubmit={handleSubmit} className="w-100">
+        <div className="d-flex flex-column mb-3">
+          <label htmlFor="name">Nombre:</label>
           <TextField
             id="name"
-            label="Nombre:"
             type="text"
             value={users?.name}
             defaultValue={users?.name}
-            onChange={(e) => setUsers({...users, name: e.target.value})}
+            onChange={(e) => setUsers({ ...users, name: e.target.value })}
           />
+        </div>
 
+        <div className="d-flex flex-column mb-3">
+          <label htmlFor="lastname">Apellido:</label>
           <TextField
             id="lastname"
-            label="Apellido: "
             type="text"
             value={users?.lastname}
             defaultValue={users?.lastname}
-            onChange={(e) => setUsers({...users, lastname: e.target.value})}
+            onChange={(e) => setUsers({ ...users, lastname: e.target.value })}
           />
+        </div>
 
+        <div className="d-flex flex-column mb-3">
+          <label htmlFor="email">Email:</label>
           <TextField
             id="email"
-            label="Email:"
             type="email"
             value={users?.email}
             defaultValue={users?.email}
-            onChange={(e) => setUsers({...users, email: e.target.value})}
+            onChange={(e) => setUsers({ ...users, email: e.target.value })}
           />
+        </div>
 
+        <div className="d-flex flex-column mb-3">
+          <label htmlFor="password">Contraseña:</label>
           <TextField
             id="password"
-            label="Contraseña:"
             type="password"
             value={users?.password}
             defaultValue={users?.password}
-            onChange={(e) => setUsers({...users, password: e.target.value})}
+            onChange={(e) => setUsers({ ...users, password: e.target.value })}
           />
+        </div>
 
-          {/* <TextField
+        {/* <div className="d-flex flex-column mb-3">
+          <label htmlFor="role">Rol:</label>
+          <TextField
             id="role"
-            label="Rol:"
             type="text"
             value={users?.role}
             defaultValue={users?.role}
-            onChange={(e) => setUsers({...users, role: e.target.value})}
-          /> */}
+            onChange={(e) => setUsers({ ...users, role: e.target.value })}
+          />
+        </div> */}
 
+        <div className="d-flex flex-column mb-3">
+          <label htmlFor="username">Nombre de usuario:</label>
           <TextField
             id="username"
-            label="Nombre de usuario:"
             type="text"
             value={users?.username}
             defaultValue={users?.username}
-            onChange={(e) => setUsers({...users, username: e.target.value})}
+            onChange={(e) => setUsers({ ...users, username: e.target.value })}
           />
+        </div>
 
-          <Button variant="primary" type="submit">
+        <div className="d-flex justify-content-end">
+          <Button variant="success" type="submit">
             Guardar
           </Button>
-          
-        </form>
-      </div>
-    </div>
-  )
-
+        </div>
+      </form>
+    </Container>
+  );
 };
 
 export default UsersAdd;
+
