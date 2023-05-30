f2bc1acea377e13c7bc2a4f1b0681336769f5505
NicoleMelendez <k.nicolemelendez@gmail.com>
2023-05-30 11:15:05 -0600

    Show navbar options based on user type

--- src/App.tsx
+++ src/App.tsx
@@ -133,7 +133,7 @@ const verifyRole = () => {
           {/* <Route path="/articles" element={<ProductCustomerHome />} /> */}
               <Route path="/about"  element={<About/>} />
               <Route path="/" element={<Banner/>} />
-              {/* <Route path="/banner" element={<Banner/>} /> */}
+              <Route path="/banner"       element={ <Navigate to="/" replace />} />
             
         <Route element={<ProtectedRoute />} >
 
